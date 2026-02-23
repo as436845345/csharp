be8f7f6a0eac2b9ccdd420d599e2acf03a74d446
ljhhhh <2570319150@qq.com>
2026-02-23 09:44:24 +0800

    实现主项目获取所有引用的程序集，并执行相应操作
    1. 在主项目可执行程序的目录下获取所有以 CSharp.*.dll 开头的 DLL；
    2. 通过 AssemblyLoadContext.Default.LoadFromAssemblyPath 加载程序集；
    3. 获取所有含 DisplayAttribute 的类；
    4. 执行这些类的 Execute 静态函数。

--- src/CSharp/CSharp/Program.cs
+++ src/CSharp/CSharp/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Runtime.Loader;
+
+// 主动加载所有相关程序集
+var dllFiles = Directory.GetFiles(
+    AppContext.BaseDirectory,
+    "CSharp.*.dll",
+    SearchOption.TopDirectoryOnly);
+
+foreach (var dll in dllFiles)
+{
+    try
+    {
+        AssemblyLoadContext.Default.LoadFromAssemblyPath(dll);
+    }
+    catch { }
+}
+
+// 现在再获取所有程序集
+var csharpAssemblies = AppDomain.CurrentDomain.GetAssemblies()
+    .Where(assembly =>
+    {
+        var assemblyName = assembly.GetName().Name;
+        return assemblyName != null && assemblyName.AsSpan().StartsWith("CSharp.");
+    });
+
+// 获取所有带有DisplayAttribute的类
+var classesWithDisplayAttribute = csharpAssemblies
+    .SelectMany(assembly => assembly.GetTypes())
+    .Where(type => type.GetCustomAttributes(typeof(DisplayAttribute), false).Length != 0);
+
+// 执行每个类的静态Execute方法
+foreach (var type in classesWithDisplayAttribute)
+{
+    var executeMethod = type.GetMethod("Execute", BindingFlags.Public | BindingFlags.Static);
+    if (executeMethod != null)
+    {
+        try
+        {
+            executeMethod.Invoke(null, null);
+            Console.WriteLine($"Executed {type.FullName}.Execute()");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error executing {type.FullName}.Execute(): {ex.Message}");
+        }
+
+        Console.WriteLine();
+    }
+}
